58f9689f220c763beab97e482269cbac2ff72fed
bt4it <bt4it2@gmail.com>
2026-07-17 17:45:40 +0200

    init Tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "instance_type-typ1" {
+  description = "The EC2 instance type (-typ1)"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_count-typ1" {
+  description = "The EC2 instance count (typ1)"
+  type        = number
+  default     = 1
+}  
+
+variable "instance_type-typ2" {
+  description = "The EC2 instance type (-typ2)"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_count-typ2" {
+  description = "The EC2 instance count (typ2)"
+  type        = number
+  default     = 1
+}  
