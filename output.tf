233c8bcc5358ea30fe17f97f18edbe9d897edacc
bt4it <bt4it2@gmail.com>
2026-07-17 18:20:35 +0200

    202

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "public_ips_typ-1" {
+output "public_ips_typ1" {
   description = "Public IP s of EC2 instances (typ1)"
   value       = aws_instance.instanz_typ1[*].public_ip
 }
 
-output "public_ips_typ-2" {
+output "public_ips_typ2" {
   description = "Public IP s of EC2 instances (typ2)"
   value       = aws_instance.instanz_typ2[*].public_ip
 }
