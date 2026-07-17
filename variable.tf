31fb7817eb24a0d561b3961f23e301b01eaded5e
bt4it <bt4it2@gmail.com>
2026-07-17 23:04:24 +0200

    v0.0.1: tf Infra with SG &  Monitoring 501

--- variable.tf
+++ variable.tf
@@ -7,7 +7,7 @@ variable "instance_type_typ1" {
 variable "instance_count_typ1" {
   description = "The EC2 instance count (typ1)"
   type        = number
-  default     = 1
+  default     = 2
 }
 
 variable "instance_type_typ2" {
@@ -20,4 +20,16 @@ variable "instance_count_typ2" {
   description = "The EC2 instance count (typ2)"
   type        = number
   default     = 1
-}  
+}
+
+variable "sg_typ1" {
+  description = "The Security Group (typ1)"
+  type        = string
+  default     = "sg-083cc6502821a7547"
+}
+
+variable "sg_typ2" {
+  description = "The EC2 instance count (typ1)"
+  type        = string
+  default     = "sg-04ed9bebdb357bf1c"
+}
